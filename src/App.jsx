d0a90bca05a205336159adf4924ffbd9b603a669
louisAKIRA <louis.saga5@gmail.com>
2023-09-21 18:12:32 +0700

    Add name

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more hi
       </p>
+      <p className="read-the-docs">
+        I'm Ittipon Chaiworn
+      </p>
     </>
   )
 }
